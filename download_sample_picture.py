7a29671ca8790fcd57ba7b470ca44b38f70a7242
dai-ichiro <d.a.i-ichiro@outlook.jp>
2022-04-08 15:20:46 +0900

    add scripts for download

---
+++ download_sample_picture.py
@@ -0,0 +1,3 @@
+from gdown import download
+
+download('https://drive.google.com/uc?id=192v3ziuxL58O3-EmgPLEnn--Px6Bfig3', quiet = False)
---
+++ download_sample_video.py
@@ -0,0 +1,3 @@
+from gdown import download
+
+download('https://drive.google.com/uc?id=1M7R3k0OK1BpDEbh4LhOOOvzKKE_Khe5Z', quiet = False)
